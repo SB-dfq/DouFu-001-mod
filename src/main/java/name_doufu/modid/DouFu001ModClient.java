6dd3e5c6d42516c96980cdad80894e21cb718a8f
dfq001 <ss80191@qq.com>
2024-07-31 16:06:15 +0800

    Initial command

---
+++ src/main/java/name_doufu/modid/DouFu001ModClient.java
@@ -0,0 +1,10 @@
+package name_doufu.modidMod
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DouFu001ModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/name_doufu/modid/DouFu001ModDataGenerator.java
@@ -0,0 +1,11 @@
+package name_doufu.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DouFu001ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
